d16ae4ee068508a19f2a4b09046bedf7f3103be8
samratrocks <samrat.shiwakoti@gmail.com>
2026-07-25 21:07:10 -0400

    add cstr exercises.

--- cstr/cstr.c
+++ cstr/cstr.c
@@ -4,14 +4,35 @@
 size_t
 my_strlen(const char *s)
 {
-	return (0);
+	size_t size = 0;
+
+	while (*s != '\0') {
+		s++;
+		size++;
+	}
+
+	return (size);
 }
 
 /* Copy src into dst byte by byte, including the '\0'. Return dst. */
 char *
 my_strcpy(char *dst, const char *src)
 {
-	return (dst);
+	/* Iterate one at a time
+	 * set the value to the dereference operations
+	 * once it works for one it should work for all
+	*/
+	char *start = dst;
+	while (*src != '\0')
+	{
+		*dst = *src;	/* Here im assinging the value of src to dst */
+		src++;
+		dst++;
+	}
+
+	*dst = '\0';
+
+	return (start);
 }
 
 /* Copy exactly n raw bytes from src to dst. Return dst. */
@@ -27,3 +48,4 @@ my_strcmp(const char *a, const char *b)
 {
 	return (0);
 }
+ 
